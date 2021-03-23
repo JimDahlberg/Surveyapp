5d7b3c9774afd66e2b9143d28fe1678a30eb7fa2
JimDahlberg <47150820+JimDahlberg@users.noreply.github.com>
2021-03-23 21:05:00 +0100

    Add files via upload

---
+++ SurveyApplication/surveyapp/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = b'\xe4\x90\x1a\x02\xdf\xd0\x7fj\x08\x8b\xa0\xb9!NI&'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+loginManager = LoginManager(app)
+
+from surveyapp import routes
